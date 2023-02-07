8df2704102d580fcd23cb66993e5a0c23162d650
NiaroAndriam <120553843+NiaroAndriam@users.noreply.github.com>
2023-02-07 15:18:39 +0300

    Update page.php

--- ci306/application/controllers/page.php
+++ ci306/application/controllers/page.php
@@ -5,7 +5,27 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
-		
+		$this->load->view('page/#');
+	}
+
+	public function login()
+	{
+		$mail = $this->input->post("mail");
+		$pass = $this->input->post("pwd");
+
+		$this->load->model('Model');
+		if($this->Model->checkLogin($mail,$pass))
+		{
+			$this->session->set_userdata('mail', $mail);
+			redirect('#');
+		}else{
+			redirect('#');
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('#');
 	}		
 }
