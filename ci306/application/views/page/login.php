2354668beb940feb6d13d7a58febdf7d221dd745
NiaroAndriam <120553843+NiaroAndriam@users.noreply.github.com>
2023-02-07 21:21:54 +0300

    bootstrap

--- ci306/application/views/page/login.php
+++ ci306/application/views/page/login.php
@@ -10,6 +10,7 @@
 <body>
     <div id="page">
         <div id="content">
+            <h3>Login</h3>
             <form action="<?php echo base_url('index.php/log/login');?>" method="post">
                 <input type="email" name="mail" placeholder="Email" ><br>
                 <input type="password" name="pwd" placeholder="Password" >
