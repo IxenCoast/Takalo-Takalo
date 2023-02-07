279275e4ade3e7e8cf24323a4de90b6ff6efe088
IxenCoast <ixencoast@gmail.com>
2023-02-07 14:19:30 +0100

    Update takalo.sql

--- takalo.sql
+++ takalo.sql
@@ -5,4 +5,4 @@ CREATE TABLE user(idu int NOT null AUTO_INCREMENT PRIMARY key, mail varchar(255)
 
 create table categorie(idc int primary key, nom varchar(255));
 
-create TABLE produit(idp int PRIMARY key not null, nom varchar(255), idu int, idc int , FOREIGN key (idu) REFERENCES user(idu), FOREIGN key (idc) REFERENCES categorie(idc));
+create TABLE produit(idp int PRIMARY key not null, nom varchar(255), idu int, idc int ,img varchar(255), FOREIGN key (idu) REFERENCES user(idu), FOREIGN key (idc) REFERENCES categorie(idc));
